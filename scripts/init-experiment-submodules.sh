7e1e3ec5da843b781eb13d2bf90df6f32a3b5326
Stefan Marr <git@stefan-marr.de>
2014-09-22 18:31:15 +0200

    Updated the script to get the new experiments
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/init-experiment-submodules.sh
+++ scripts/init-experiment-submodules.sh
@@ -9,18 +9,20 @@ INFO Setup all TruffleSOM Experiments
 
 cd TruffleSOM
 
+BRANCH_PREFIX="mt-vs-pe"
 PREFIX="T-"
 REPO_URL="git@github.com:smarr/TruffleSOM.git"
 
-git ls-remote --heads | grep opt-exp | cut -c 53- | while read branch
+git ls-remote --heads | grep $BRANCH_PREFIX | cut -c 53- | while read branch
 do
   INFO Branch $branch
-  shortName=${branch/opt-exp\//}
+  shortName=${branch/${BRANCH_PREFIX}\//}
   cd $DIR/../
   EXP_NAME="implementations/$PREFIX$shortName"
+  
   if [ ! -d "$EXP_NAME" ]; then
     INFO "Add submodule for $branch"
-    git submodule add $REPO_URL $EXP_NAME
+    git submodule add --force  $REPO_URL $EXP_NAME
     git submodule init $EXP_NAME
     git submodule update $EXP_NAME
     OK "Submodule added for $branch"
