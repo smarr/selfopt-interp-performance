931847ac73be95b98384bb4e4f7e0e0e0cad1556
Stefan Marr <git@stefan-marr.de>
2014-09-22 18:05:01 +0200

    Added script that generated the submodules
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ scripts/init-experiment-submodules.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR/../implementations/
+source script.inc
+
+INFO Setup all TruffleSOM Experiments
+
+cd TruffleSOM
+
+PREFIX="T-"
+REPO_URL="git@github.com:smarr/TruffleSOM.git"
+
+git ls-remote --heads | grep opt-exp | cut -c 53- | while read branch
+do
+  INFO Branch $branch
+  shortName=${branch/opt-exp\//}
+  cd $DIR/../
+  EXP_NAME="implementations/$PREFIX$shortName"
+  if [ ! -d "$EXP_NAME" ]; then
+    INFO "Add submodule for $branch"
+    git submodule add $REPO_URL $EXP_NAME
+    git submodule init $EXP_NAME
+    git submodule update $EXP_NAME
+    OK "Submodule added for $branch"
+  fi
+  
+  INFO Update to latest version
+  cd $EXP_NAME
+  git fetch
+  git checkout $branch
+  OK "$branch updated"
+done
