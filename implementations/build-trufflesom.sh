d8ea3504dfe980bcf22502d456ba5a7e4bbfc203
Stefan Marr <git@stefan-marr.de>
2014-09-23 10:45:29 +0200

    Extended script to build all branches/experiments
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-trufflesom.sh
+++ implementations/build-trufflesom.sh
@@ -3,8 +3,26 @@
 source ./script.inc
 source ./config.inc
 
-INFO Build TruffleSOM
+INFO Build TruffleSOM master
 cd TruffleSOM
-make clean; make
-cp ../graal/truffle.jar libs/
-OK TruffleSOM Build Completed.
+make clean
+mkdir libs; cp ../graal/build/truffle*.jar libs/
+make || {
+  ERR Build of master failed.
+  exit 1
+}
+cd ..
+OK TruffleSOM master Build Completed.
+
+for exp in $( ls -d T-* ); do
+  INFO Build TruffleSOM $exp
+  cd $exp
+  make clean
+  mkdir libs; cp ../graal/build/truffle*.jar libs/
+  make || {
+    ERR Build of $exp failed.
+    exit 1
+  }
+  cd ..
+  OK TruffleSOM $exp Build Completed.
+done
