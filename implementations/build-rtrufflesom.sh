ecefa4f4867c8a4a0b98f5624b984850073f373c
Stefan Marr <git@stefan-marr.de>
2014-10-02 13:17:56 +0200

    Build RTruffleSOM experiments in parallel
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-rtrufflesom.sh
+++ implementations/build-rtrufflesom.sh
@@ -3,11 +3,53 @@
 source ./script.inc
 source ./config.inc
 
-cd RTruffleSOM
 
+# download PyPy
 get_pypy
+export PYPY_DIR=`pwd`/pypy
 
-INFO Build RTruffleSOM
-make clean
-make -j4
-OK RTruffleSOM Build Completed.
+PAR_JOBS=8
+declare -a job_pids
+
+enq_job() {
+  elm=$1
+  job_pids=("${job_pids[@]}" "$elm")
+}
+
+deq_job() {
+  dequed_job=${job_pids[0]}
+  unset job_pids[0]
+  job_pids=("${job_pids[@]}")
+}
+
+
+build_branch() {
+  exp="$1"
+  INFO Build RTruffleSOM $exp
+  
+  cd $exp
+  make clean > /dev/null
+  make -j4 > /dev/null || {
+    ERR Build of $exp failed.
+    exit 1
+  }
+  cd ..
+  OK RTruffleSOM $exp Build Completed.
+}
+
+build_branch "RTruffleSOM" &
+enq_job $!
+
+((PAR_JOBS--))
+
+for exp in $( ls -d R-* ); do
+  build_branch "$exp" &
+  enq_job $!
+  ((PAR_JOBS--))
+  if [ $PAR_JOBS -eq 0 ]; then
+    deq_job
+    wait $dequed_job
+    ((PAR_JOBS++))
+  fi
+done
+wait
