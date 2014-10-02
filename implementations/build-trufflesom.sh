58b664901995af1c0c3978ff5bcb2512c788e095
Stefan Marr <git@stefan-marr.de>
2014-10-02 13:11:49 +0200

    Make building TruffleSOMs parallel
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-trufflesom.sh
+++ implementations/build-trufflesom.sh
@@ -3,26 +3,48 @@
 source ./script.inc
 source ./config.inc
 
-INFO Build TruffleSOM master
-cd TruffleSOM
-make clean
-mkdir libs; cp ../graal/build/truffle*.jar libs/
-make || {
-  ERR Build of master failed.
-  exit 1
-}
-cd ..
-OK TruffleSOM master Build Completed.
 
-for exp in $( ls -d T-* ); do
+PAR_JOBS=4
+declare -a job_pids
+
+enq_job() {
+  elm=$1
+  job_pids=("${job_pids[@]}" "$elm")
+}
+
+deq_job() {
+  dequed_job=${job_pids[0]}
+  unset job_pids[0]
+  job_pids=("${job_pids[@]}")
+}
+
+build_branch() {
+  exp="$1"
   INFO Build TruffleSOM $exp
   cd $exp
-  make clean
+  make clean > /dev/null
   mkdir libs; cp ../graal/build/truffle*.jar libs/
-  make || {
+  make > /dev/null || {
     ERR Build of $exp failed.
     exit 1
   }
   cd ..
   OK TruffleSOM $exp Build Completed.
+}
+
+build_branch "TruffleSOM" &
+enq_job $!
+
+((PAR_JOBS--))
+
+for exp in $( ls -d T-* ); do
+  build_branch "$exp" &
+  enq_job $!
+  ((PAR_JOBS--))
+  if [ $PAR_JOBS -eq 0 ]; then
+    deq_job
+    wait $dequed_job
+    ((PAR_JOBS++))
+  fi
 done
+wait
