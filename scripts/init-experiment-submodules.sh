05ba278a0dc0beedd19a6f8baa99432639459dc1
Stefan Marr <git@stefan-marr.de>
2014-10-02 10:54:24 +0200

    Added RTruffleSOM experiment submodules
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/init-experiment-submodules.sh
+++ scripts/init-experiment-submodules.sh
@@ -5,32 +5,38 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../implementations/
 source script.inc
 
+create_submodules_for_experiments() {
+  REPO_URL="$1"
+  BRANCH_PREFIX="$2"
+  PREFIX="$3"
+  
+  git ls-remote --heads | grep $BRANCH_PREFIX | cut -c 53- | while read branch
+  do
+    INFO Branch $branch
+    shortName=${branch/${BRANCH_PREFIX}\//}
+    cd $DIR/../
+    EXP_NAME="implementations/$PREFIX$shortName"
+  
+    if [ ! -d "$EXP_NAME" ]; then
+      INFO "Add submodule for $branch"
+      git submodule add --force  $REPO_URL $EXP_NAME
+      git submodule init $EXP_NAME
+      git submodule update $EXP_NAME
+      OK "Submodule added for $branch"
+    fi
+  
+    INFO Update to latest version
+    cd $EXP_NAME
+    git fetch
+    git checkout $branch
+    OK "$branch updated"
+  done
+}
+
 INFO Setup all TruffleSOM Experiments
 
 cd TruffleSOM
+create_submodules_for_experiments "https://github.com/smarr/TruffleSOM.git" "mt-vs-pe" "T-"
 
-BRANCH_PREFIX="mt-vs-pe"
-PREFIX="T-"
-REPO_URL="git@github.com:smarr/TruffleSOM.git"
-
-git ls-remote --heads | grep $BRANCH_PREFIX | cut -c 53- | while read branch
-do
-  INFO Branch $branch
-  shortName=${branch/${BRANCH_PREFIX}\//}
-  cd $DIR/../
-  EXP_NAME="implementations/$PREFIX$shortName"
-  
-  if [ ! -d "$EXP_NAME" ]; then
-    INFO "Add submodule for $branch"
-    git submodule add --force  $REPO_URL $EXP_NAME
-    git submodule init $EXP_NAME
-    git submodule update $EXP_NAME
-    OK "Submodule added for $branch"
-  fi
-  
-  INFO Update to latest version
-  cd $EXP_NAME
-  git fetch
-  git checkout $branch
-  OK "$branch updated"
-done
+cd ../RTruffleSOM
+create_submodules_for_experiments "https://github.com/smarr/RTruffleSOM.git" "mt-vs-pe" "R-"
