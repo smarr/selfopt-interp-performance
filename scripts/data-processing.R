949e758abf965e18d0a9156d4ae2f633f13fb366
Stefan Marr <git@stefan-marr.de>
2014-10-25 11:51:30 +0200

    Added experiment name mapping
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/data-processing.R
+++ scripts/data-processing.R
@@ -23,6 +23,25 @@ prepare_vm_names <- function(data) {
   data
 }
 
+prepare_exp_names <- function(data) {
+    name_map <- list("baseline"                               = "baseline",
+                     "without-args-in-frame"                  = "typed args",
+                     "without-blocks-without-context"         = "min. escaping closures",
+                     "without-catch-nonlocal-return-node"     = "catch-return nodes",
+                     "without-control-specialization"         = "lower control structures",
+                     "without-custom-PICs"                    = "inline caching",
+                     "without-eager-primitives"               = "inline basic ops.",
+                     "without-execute-void"                   = "opt. side-effect-free",
+                     "without-global-caching-and-opt"         = "cache globals",
+                     "without-local-nonlocal-var-distinction" = "opt. local vars",
+                     "without-object-layout"                  = "typed fields",
+                     "without-splitting-frame-variables"      = "min. escaping vars",
+                     "without-unessential-lowering-prims"     = "lower common ops",
+                     "without-var-access-specialization"      = "typed vars")
+    levels(data$Var) <- map_names(levels(data$Var), name_map)
+    data
+}
+
 map_names <- function(old_names, name_map) {
   for (i in 1:length(old_names)) {
     old_name <- old_names[[i]]
