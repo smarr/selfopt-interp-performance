74f9e93065dbf20ee0c5d6b04f285099dcc738c2
Stefan Marr <git@stefan-marr.de>
2014-12-05 13:54:43 +0100

    Change patch-statistics script to generate proper CSV files
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/patch-statistics.sh
+++ scripts/patch-statistics.sh
@@ -11,17 +11,23 @@ collect_stats() {
 
   # Clear files
   echo "" > patch-stats.txt 
-  echo "" > cloc.txt
+  echo "experiment,files,language,blank,comment,code" > cloc.txt
 
   # git ls-remote --heads
   git ls-remote . | grep $BRANCH_PREFIX | cut -c 62- | while read branch
   do
     INFO Branch $branch
     shortName=${branch/${BRANCH_PREFIX}\//}
-    echo -ne "$shortName:\t\t" >> patch-stats.txt
-    echo -ne "$shortName:\t\t" >> cloc.txt
+    echo -ne "$shortName,\t\t" >> patch-stats.txt
+    echo -ne "$shortName,\t\t" >> cloc.txt
     git diff --shortstat remotes/origin/$BRANCH_PREFIX/baseline..remotes/origin/$branch >> patch-stats.txt
-    ../../scripts/cloc --quiet --csv ../$EXP_PREFIX$shortName/src >> cloc.txt
+    ../../scripts/cloc --quiet --csv ../$EXP_PREFIX$shortName/src | tr -d '\n' >> cloc.txt
+    echo -e "" >> cloc.txt
+    
+    if [[ "$BRANCH_PREFIX/baseline" == $branch ]]
+    then
+      echo -e "" >> patch-stats.txt
+    fi
   done
 }
 
