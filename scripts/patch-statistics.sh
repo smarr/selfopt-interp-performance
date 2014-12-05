6c9c0c610929d47b99f8bb01650a86ec723a6d10
Stefan Marr <git@stefan-marr.de>
2014-12-05 14:01:57 +0100

    Generate files, combining data for both VMs
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/patch-statistics.sh
+++ scripts/patch-statistics.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATA_CLOC=$DIR/../data/cloc.csv
+DATA_PATCH=$DIR/../data/patch-stats.csv
 
 cd $DIR/../implementations/
 source script.inc
@@ -8,33 +10,35 @@ source script.inc
 collect_stats() {
   BRANCH_PREFIX="$1"
   EXP_PREFIX="$2"
-
-  # Clear files
-  echo "" > patch-stats.txt 
-  echo "experiment,files,language,blank,comment,code" > cloc.txt
+  VM="$3"
 
   # git ls-remote --heads
   git ls-remote . | grep $BRANCH_PREFIX | cut -c 62- | while read branch
   do
     INFO Branch $branch
     shortName=${branch/${BRANCH_PREFIX}\//}
-    echo -ne "$shortName,\t\t" >> patch-stats.txt
-    echo -ne "$shortName,\t\t" >> cloc.txt
-    git diff --shortstat remotes/origin/$BRANCH_PREFIX/baseline..remotes/origin/$branch >> patch-stats.txt
-    ../../scripts/cloc --quiet --csv ../$EXP_PREFIX$shortName/src | tr -d '\n' >> cloc.txt
-    echo -e "" >> cloc.txt
+    echo -ne "$VM,$shortName,\t\t" >> $DATA_PATCH
+    echo -ne "$VM,$shortName,\t\t" >> $DATA_CLOC
+    git diff --shortstat remotes/origin/$BRANCH_PREFIX/baseline..remotes/origin/$branch >> $DATA_PATCH
+    ../../scripts/cloc --quiet --csv ../$EXP_PREFIX$shortName/src | tr -d '\n' >> $DATA_CLOC
+    echo -e "" >> $DATA_CLOC
     
     if [[ "$BRANCH_PREFIX/baseline" == $branch ]]
     then
-      echo -e "" >> patch-stats.txt
+      echo -e "" >> $DATA_PATCH
     fi
   done
 }
 
 INFO "Collect patch-stats.txt for experiments"
 
+# Clear files
+echo "" > $DATA_PATCH
+echo "VM,experiment,files,language,blank,comment,code" > $DATA_CLOC
+
+
 cd TruffleSOM
-collect_stats "mt-vs-pe" "T-"
+collect_stats "mt-vs-pe" "T-" "TruffleSOM"
 
 cd ../RTruffleSOM
-collect_stats "mt-vs-pe" "R-"
+collect_stats "mt-vs-pe" "R-" "RTruffleSOM"
