d7bb4d3b2027b9f0ddfed44ecbc0a4366a657937
Stefan Marr <git@stefan-marr.de>
2015-09-01 16:03:45 +0200

    Remove old stuff from building
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/setup.sh
+++ implementations/setup.sh
@@ -8,14 +8,6 @@ get_web_getter
 
 ./build-graal.sh
 ./build-classic-benchmarks.sh
-# ./build-rpysom.sh
-./build-trufflesom.sh
-./build-rtrufflesom.sh
-# ./build-jruby.sh
-# ./build-sompp.sh
-./build-som.sh
 ./build-somns.sh
-# ./build-csom.sh
-# ./build-luajit.sh
 
 OK done.
