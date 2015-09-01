bf3854e890b4809cf4b4fad87fd6f1ea523594ec
Stefan Marr <git@stefan-marr.de>
2015-09-01 23:53:18 +0200

    Put the results in a persistent location
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 mkdir -p data
 rebench -d --scheduler=random --without-nice rebench.conf all
+REV=`git rev-parse HEAD | cut -c1-8`
+TARGET_PATH=~/benchmark-results/somns-agere/$REV
+mkdir -p $TARGET_PATH
+cp data/* $TARGET_PATH/
