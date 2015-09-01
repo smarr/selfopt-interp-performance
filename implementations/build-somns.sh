c6e05be7bbcd14e319fca1c5b94872f8544b1b31
Stefan Marr <git@stefan-marr.de>
2015-09-01 16:05:03 +0200

    Build both SOMns versions
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-somns.sh
+++ implementations/build-somns.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+set -e
 
 source ./script.inc
 source ./config.inc
 
-INFO Build SOMns
+INFO "Build SOMns"
 cd SOMns
 make clean; make
-OK SOMns Build Completed.
+cd ..
+OK "SOMns: Build Completed."
+
+INFO "Build SOMns without optimizations"
+cd SOMns-wo-opt
+make clean; make
+OK "SOMns without optimizations: Build Completed."
