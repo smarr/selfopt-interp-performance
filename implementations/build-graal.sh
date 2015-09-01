4b3fefa173129d8651e62b2955669c8a715c1282
Stefan Marr <git@stefan-marr.de>
2015-09-01 15:40:54 +0200

    Change settings to build Graal VM for comparison to Java
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-graal.sh
+++ implementations/build-graal.sh
@@ -7,16 +7,14 @@ source ./config.inc
 if [ $? -eq 0 ]; then
     # we have to make clang compile graal gracefully
     export COMPILER_WARNINGS_FATAL=false
-    export USE_PRECOMPILED_HEADER=0
     export USE_CLANG=true
 fi
-export DEFAULT_VM=server
+export DEFAULT_VM=graal
 export JAVA_HOME=$JAVA8_HOME
 export EXTRA_JAVA_HOMES=$JAVA7_HOME
 
 INFO Build Graal
 cd graal
 ./mx.sh clean
-./mx.sh --vm server build -p
-#./mx.sh --vm server trufflejar
+./mx.sh --vm graal build -p
 OK Graal Build Completed.
