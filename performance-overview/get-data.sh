528f4d9f115d49ec81e5a6d91dc3a96cff6d3d4d
Stefan Marr <git@stefan-marr.de>
2015-09-01 15:24:19 +0200

    Updated get-data
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- performance-overview/get-data.sh
+++ performance-overview/get-data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp 8:~/Projects/selfopt-interp-performance/performance-overview/perf-overview.data .
+# scp 8:~/Projects/selfopt-interp-performance/performance-overview/*.data .
+scp 8:Projects/RoarVM-buildslave/selfopt-interp-performance/build/data/*.data .
