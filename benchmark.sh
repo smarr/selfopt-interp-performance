e652a18164237dd34e1cb2c9b767c14cad71a382
Stefan Marr <git@stefan-marr.de>
2015-09-03 11:52:35 +0200

    Clear out old results
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p data
 #rebench -f -d --scheduler=random --without-nice rebench.conf all
-rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-actor
+rebench -c -f -d --scheduler=random --without-nice rebench.conf SOMns-actor
 
 REV=`git rev-parse HEAD | cut -c1-8`
 TARGET_PATH=~/benchmark-results/somns-agere/$REV
