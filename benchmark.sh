9ad1b25d9c66d7d528e69e8035f0e2ccf195c500
Stefan Marr <git@stefan-marr.de>
2015-09-05 12:55:06 +0200

    Added microbenchmark, let's just run that one
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p data
 #rebench -f -d --scheduler=random --without-nice rebench.conf all
-rebench -c -f -d --scheduler=random --without-nice rebench.conf SOMns-micro
+rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-micro
 
 REV=`git rev-parse HEAD | cut -c1-8`
 TARGET_PATH=~/benchmark-results/somns-agere/$REV
