bd63170c837b5205f40440e4686eb2e80eb471b8
Stefan Marr <git@stefan-marr.de>
2015-09-24 14:54:01 +0200

    Remove discarding of data
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,8 @@
 mkdir -p data
 #rebench -f -d --scheduler=random --without-nice rebench.conf all
 #rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-micro
-rebench -c -f -d --scheduler=random --without-nice rebench.conf SOMns-jit
-rebench -c -f -d --scheduler=random --without-nice rebench.conf Java
+rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-jit
+rebench -f -d --scheduler=random --without-nice rebench.conf Java
 
 REV=`git rev-parse HEAD | cut -c1-8`
 TARGET_PATH=~/benchmark-results/somns-agere/$REV
