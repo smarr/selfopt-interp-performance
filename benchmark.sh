98967078da32652c990dbfd6443d0bb0b267f2e9
Stefan Marr <git@stefan-marr.de>
2015-10-15 19:54:49 +0200

    Only run currently relevant benchmarks
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 mkdir -p data
-rebench -f -d --scheduler=random --without-nice rebench.conf all
-#rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-micro
+#rebench -f -d --scheduler=random --without-nice rebench.conf all
+rebench -f -d --scheduler=random --without-nice rebench.conf Savina
+rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-actor
+rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-micro
 #rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-jit
 #rebench -f -d --scheduler=random --without-nice rebench.conf Java
 
