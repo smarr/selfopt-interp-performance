3ad67b61b0280838fc944f687a9ac3195d3a8d1a
Stefan Marr <git@stefan-marr.de>
2015-10-15 17:56:24 +0200

    Number the directories so that it is easy to find the latest one
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -5,7 +5,12 @@ mkdir -p data
 rebench -f -d --scheduler=random --without-nice rebench.conf SOMns-jit
 rebench -f -d --scheduler=random --without-nice rebench.conf Java
 
+DATA_ROOT=~/benchmark-results/somns-agere
+
+NUM_PREV=`ls -l | grep ^d | wc -l`
+NUM_PREV=`printf "%03d" $NUM_PREV`
+
 REV=`git rev-parse HEAD | cut -c1-8`
-TARGET_PATH=~/benchmark-results/somns-agere/$REV
+TARGET_PATH=$DATA_ROOT/$NUM_PREV-$REV
 mkdir -p $TARGET_PATH
 cp data/* $TARGET_PATH/
